d81b9dbc46f31764a51a26d4b9b5ef1c8909cf22
Alejandra Olaya Casanova <maolaya15@gmail.com>
2020-12-12 18:00:08 -0500

    modificacion main.js y router.js

--- src/main.js
+++ src/main.js
@@ -5,9 +5,9 @@ import App from './App'
 import vueRouter from 'vue-router'
 
 import router from './router'
-Vue.use(vueRouter)
+Vue.use(vueRouter);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
--- src/router.js
+++ src/router.js
@@ -4,24 +4,24 @@ import UserBalance from './components/UserBalance'
 import App from './App'
 
 const router = new vueRouter({
-        mode: 'history',
-        base: __dirname,
-        routes: [
-            {
-                path: '/',
-                name: "root",
-                component: App
-            },
-            {
-                path: '/user/:username',
-                name: "user",
-                component: User
-            },
-            {
-                path: '/user/balance/:username',
-                name: "user_balance",
-                component: UserBalance
-            },
-        ]
-    })
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        {
+            path: '/',
+            name: "root",
+            component: App
+        },
+        {
+            path: '/user/:username',
+            name: "user",
+            component: User
+        },
+        {
+            path: '/user/balance/:username',
+            name: "user_balance",
+            component: UserBalance
+        }
+    ]
+});
 export default router
